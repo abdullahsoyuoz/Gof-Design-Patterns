9b3e1f89670180e9b4972c8453241058c3f6b645
abdullahsoyuoz <abdullahsoyuoz@gmail.com>
2020-09-17 02:13:28 +0300

    guncelleme

--- 08 - Composite - Structural Pattern/Program.cs
+++ 08 - Composite - Structural Pattern/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace _08___Composite___Structural_Pattern
 {
@@ -7,6 +9,28 @@ namespace _08___Composite___Structural_Pattern
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface IModel
+    {
+        public int _id { get; set; }
+        public string _name { get; set; }   //etc
+    }
+
+    class TypeA : IModel, IEnumerable<IModel>
+    {
+        public int _id { get; set; }
+        public string _name { get; set; }
+
+        public IEnumerator<IModel> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
