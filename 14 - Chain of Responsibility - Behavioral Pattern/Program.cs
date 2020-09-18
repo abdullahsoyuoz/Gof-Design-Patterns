504a25f3e253ee20cc12740b969aa7eb8449851b
abdullahsoyuoz <abdullahsoyuoz@gmail.com>
2020-09-18 05:38:05 +0300

    guncelleme

--- 14 - Chain of Responsibility - Behavioral Pattern/Program.cs
+++ 14 - Chain of Responsibility - Behavioral Pattern/Program.cs
@@ -2,11 +2,76 @@
 
 namespace _14___Chain_of_Responsibility___Behavioral_Pattern
 {
+
+    /*
+     Bunun açıklamasını yapmayı unutma
+     
+     */
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Junior Jack = new Junior(new Person("Jack", 45));
+            Senior Chris = new Senior(new Person("Chris", 95));
+
+            Jack.SetSenior(Chris);
+            Jack.Handle();
         }
     }
+
+    class Person
+    {
+        private string _name;
+        public string name { get => _name; set => _name = name; }
+
+        private int _strength { get; set; }
+        public int strength { get => _strength; set => _strength = strength; }
+
+        public Person(string name, int strength)
+        {
+            this._name = name;
+            this._strength = strength;
+        }
+    }
+
+    abstract class PositionBase
+    {
+        protected PositionBase _senior;
+        public void SetSenior(PositionBase senior) => this._senior = senior;
+        public abstract void Handle();
+    }
+    class Junior : PositionBase
+    {
+        public Person person;
+        public override void Handle()
+        {
+            if (person.strength < 50)
+            {
+                _senior.Handle();
+            }
+            else if (person != null)
+                Console.WriteLine(person.name + " bu işi yapar !");
+        }
+
+        public Junior(Person person)
+        {
+            this.person = person;
+        }
+    }
+
+    class Senior : PositionBase
+    {
+        public Person person;
+        public override void Handle()
+        {
+            Console.WriteLine(person.name + " bu işi yapar !");
+        }
+
+        public Senior(Person person)
+        {
+            this.person = person;
+        }
+    }
+
 }
---
+++ 15 - Memento - Behavioral Pattern/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _15___Memento___Behavioral_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yeni bir Kitap !");
+            Model bookA = new Model("Halide Edip Adıvar","Ateşten Gömlek",253);
+            Console.WriteLine(bookA);
+
+            Taker taker = new Taker();
+            taker._yedek = bookA.Recover();
+
+            Console.WriteLine("\nManipüle Ettik.");
+            bookA._title = "Sinekli Bakkal";
+            bookA._pagenumber = 424;
+            Console.WriteLine(bookA);
+
+            Console.WriteLine("\nAsıl bilgilere restore ettik !");
+            bookA.Restore(taker._yedek);
+            Console.WriteLine(bookA);
+        }
+    }
+
+    class Model
+    {
+        public string _author;
+        public string _title;
+        public int _pagenumber;
+        public DateTime _lastEdit;
+        public Model(string Author, string Title, int PageNumber)
+        {
+            this._author = Author;
+            this._title = Title;
+            this._pagenumber = PageNumber;
+            getLastEdited();
+        }
+
+        public void Restore(Yedek model)
+        {
+            this._author = model.Author;
+            this._title = model.Title;
+            this._pagenumber = model.PageNumber;
+        }
+        public Yedek Recover() => new Yedek(this._author, this._title, this._pagenumber, this._lastEdit);
+        public void getLastEdited() => _lastEdit = DateTime.UtcNow;
+        public override string ToString() => "Title: "+this._title+", Author: "+this._author+", PageNumber: "+this._pagenumber+" & Last Edit Time: "+this._lastEdit;
+        
+    }
+    class Yedek //  Memento
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int PageNumber { get; set; }
+        public DateTime _lastEdited { get; set; }
+
+        public Yedek(string Author, string Title, int PageNumber, DateTime _lastedited)
+        {
+            this.Author = Author;
+            this.Title = Title;
+            this.PageNumber = PageNumber;
+            this._lastEdited = _lastedited;
+        }
+    }
+
+    class Taker
+    {
+        public Yedek _yedek { get; set; }
+    }
+
+}
