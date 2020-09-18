17208e3f05c6200ca9a007aa869ae98319026b5d
abdullahsoyuoz <abdullahsoyuoz@gmail.com>
2020-09-18 22:10:59 +0300

    guncelleme

--- 17 - State - Behavioral Pattern/Program.cs
+++ 17 - State - Behavioral Pattern/Program.cs
@@ -6,7 +6,55 @@ namespace _17___State___Behavioral_Pattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusicPlayer musicplayer = new MusicPlayer();
+
+            Console.WriteLine("1 for play or 2 for pause");
+            int selected = int.Parse(Console.ReadLine());
+            switch (selected)
+            {
+                case 1:
+                    Play play = new Play();
+                    play.Action(musicplayer);
+                    break;
+                case 2:
+                    Pause deleted = new Pause();
+                    deleted.Action(musicplayer);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    class MusicPlayer  // müzik çalar için düşünülebilir
+    {
+        private ISampleState _state;
+        public void SetState(ISampleState _state) => this._state = _state;
+        //todo action
+    }
+
+    interface ISampleState
+    {
+        void Action(MusicPlayer service); 
+    }
+
+    class Play : ISampleState   // bunlar state'ler
+    {
+        public void Action(MusicPlayer service)
+        {
+            service.SetState(this);
+            Console.WriteLine("Music Player is playing !");
+            //todo
+        }
+    }
+
+    class Pause : ISampleState  // next, previous.. etc
+    {
+        public void Action(MusicPlayer service)
+        {
+            service.SetState(this);
+            Console.WriteLine("Music Player is paused !");
+            //todo
         }
     }
 }
---
+++ 18 - Command - Behavioral Pattern/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _18___Command___Behavioral_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+
+            ConditionA conditionA = new ConditionA(manager);
+            ConditionB conditionB = new ConditionB(manager);
+
+            OrderTracker tracker = new OrderTracker();
+            tracker.TakeOrder(conditionA);
+            tracker.TakeOrder(conditionB);
+
+            tracker.RunOrders();
+        }
+    }
+
+    class Manager
+    {
+        private int _id;
+        public int ID { get => _id; set => _id = ID; }  // like prop's
+
+        public void ConditionAProcess() 
+        {
+            //todo
+            Console.WriteLine("Condition A Processed !");
+        }
+        public void ConditionBProcess() 
+        {
+            //todo
+            Console.WriteLine("Condition B Processed !");
+        }
+        
+    }
+
+    class OrderTracker  // zinciri takip edip, işleyen
+    {
+        private List<IOrderCondition> _list;
+        public OrderTracker()
+        {
+            _list = new List<IOrderCondition>();
+        }
+
+        public void TakeOrder(IOrderCondition item) => _list.Add(item);
+        public void RunOrders()
+        {
+            foreach (var item in _list)
+            {
+                item.Execute();
+            }
+            _list.Clear();  // zincirin tüm işlemleri tamamlandı, todo list temizlendi !!
+            Console.WriteLine("\nSipariş zinciri temizlendi !!");
+        }
+    }
+
+    interface IOrderCondition
+    {
+        void Execute();
+    }
+
+    class ConditionA : IOrderCondition  // zincirleme işlemler, alış-satış yada yeni-düzenle-kaydet vbvbvb
+    {
+        private Manager _manager;
+
+        public ConditionA(Manager _manager)
+        {
+            this._manager = _manager;
+        }
+        public void Execute()
+        {
+            _manager.ConditionAProcess();
+        }
+    }
+    class ConditionB : IOrderCondition
+    {
+        private Manager _manager;
+
+        public ConditionB(Manager _manager)
+        {
+            this._manager = _manager;
+        }
+        public void Execute()
+        {
+            _manager.ConditionBProcess();
+        }
+    }
+}
