a1bd65bb8b894db939718908d51791449a49fd39
abdullahsoyuoz <abdullahsoyuoz@gmail.com>
2020-09-18 23:21:56 +0300

    guncelleme

---
+++ 19 - Visitor - Behavioral Pattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _19___Visitor___Behavioral_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ControllerA controllerA = new ControllerA();
+            SampleControlVisitor visitor = new SampleControlVisitor();
+
+            visitor.Supplier(controllerA);
+        }
+
+        abstract class SampleControl
+        {
+            protected long _id;
+            public long ID { get => _id; set => _id = ID; }
+            public abstract void Supply(IVisitor visitor);
+        }
+
+        class ControllerA : SampleControl  // like can derive.. ControlB.. etc
+        {
+            public override void Supply(IVisitor visitor)
+            {
+                visitor.Supplier(this);
+            }
+        }
+
+        interface IVisitor
+        {
+            void Supplier(SampleControl control);
+        }
+
+        class SampleControlVisitor : IVisitor
+        {
+            public void Supplier(SampleControl control)
+            {
+                if(control is ControllerA)
+                {
+                    Console.WriteLine("ControllerVisitor'un eylemi uygulandı !");
+                    //todo
+                }
+            }
+        }
+    }
+}
---
+++ 20 - Iterator - Behavioral Pattern/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20___Iterator___Behavioral_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeA a = new TypeA();
+            a.Setter(new List<SampleObject>()       // fake data
+            {
+                new SampleObject(0),
+                new SampleObject(1),
+                new SampleObject(2),
+                new SampleObject(3),
+                new SampleObject(4),
+                new SampleObject(5),
+                    
+            });
+                
+            
+            TypeB b = new TypeB(5);
+            b.Setter(new List<SampleObject>()       // fake data
+            {
+                new SampleObject(0),
+                new SampleObject(1),
+                new SampleObject(2),
+                new SampleObject(3),
+                new SampleObject(4),
+                new SampleObject(5),
+
+            });
+
+            SampleObjectIterator.GetSampleObject(a.Getter());
+            SampleObjectIterator.GetSampleObject(b.Getter());
+        }
+    }
+
+    class SampleObject
+    {
+        private int _id;
+        public int ID { get => _id; set => _id = ID; }
+        public SampleObject(int _id)
+        {
+            this._id = _id;
+        }
+        // sample functions etc
+    }
+
+    class TypeA
+    {
+        private List<SampleObject> _list;
+        public TypeA()
+        {
+            _list = new List<SampleObject>();
+            //todo
+        }
+        public SampleObjectIterator Getter() => new SampleObjectIterator(_list);
+        public void Setter(List<SampleObject> item) => this._list = item;
+    }
+
+    class TypeB
+    {
+        private SampleObject[] _list;
+        public TypeB(int lenght)
+        {
+            _list = new SampleObject[lenght];
+            //todo
+        }
+        public SampleObjectIterator Getter() => new SampleObjectIterator(_list.ToList());
+        public void Setter(List<SampleObject> item) => _list = item.ToArray();
+    }
+
+    class SampleObjectIterator : IEnumerator  //  !!
+    {
+        private List<SampleObject> _list;
+        private int index;  // default: 0
+        public SampleObjectIterator(List<SampleObject> _list)
+        {
+            this._list = _list;
+        }
+        public object Current   // implemente
+        {
+            get
+            {
+                SampleObject item = _list[index];
+                index++;
+                return item;
+            }
+        }
+        public bool MoveNext()  // implemente
+        {
+            if (index < _list.Count)
+            {
+                return true;
+            }
+            else
+                Reset();
+            return false;
+        }
+        public void Reset() => index = 0;
+        public static void GetSampleObject(IEnumerator item)
+        {
+            while(item.MoveNext())
+            {
+                SampleObject current = (SampleObject)item.Current;
+                int currentID = current.ID;
+                Console.WriteLine("\t"+current.ID);
+            }
+        }
+    }
+
+}
